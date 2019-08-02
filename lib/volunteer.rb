abbc6886e65063f8b554659b5f8a396b46a95cc5
FatherofDharma <fatherofdharma@gmail.com>
2019-08-02 12:02:52 -0700

    add all method for Volunteer with passing spec.

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -12,4 +12,15 @@ class Volunteer
     self.name == volunteer_to_compare.name
   end
 
+  def self.all
+    returned_volunteers = DB.exec("SELECT * FROM volunteers ORDER BY name;")
+    volunteers = []
+    returned_volunteers.each() do |volunteer|
+      name = volunteer.fetch('name')
+      id = volunteer.fetch('id').to_i
+      volunteers.push(Volunteer.new({:name => name, :id => id}, :project_id => 1))
+    end
+    volunteers
+  end
+
 end
